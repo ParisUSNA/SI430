9b7895854fb89625c5f76c528936dadb7d694e50
ParisUSNA <m264845@usna.edu>
2025-01-16 09:19:00 -0500

    msg

--- labs/02/httpc.py
+++ labs/02/httpc.py
@@ -14,15 +14,6 @@ except TimeoutError:
 payload = f"GET / HTTP/1.1\r\nHost: {dest}\r\nAccept:*/*\r\n\r\n"
 sock.send(payload.encode())
 
-length = 0
 data = sock.recv(1024)
 data = data.decode()
 print(data)
-split = data.split("\r\n")
-for i in split:
-    temp = i.split(":")
-    if temp[0] == "Content-Length":
-        length = int(temp[1].strip())
-
-content = sock.recv(length)
-print(content.decode())
